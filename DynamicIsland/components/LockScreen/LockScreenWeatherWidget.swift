ee1ecec033b3a5971ba66697673e659d40ea5828
Hariharan Mudaliar <hrhn.mudaliar251@gmail.com>
2025-10-19 23:18:33 +0530

    Working with settings

--- DynamicIsland/components/LockScreen/LockScreenWeatherWidget.swift
+++ DynamicIsland/components/LockScreen/LockScreenWeatherWidget.swift
@@ -12,7 +12,7 @@ struct LockScreenWeatherWidget: View {
                 .font(.system(size: 24, weight: .semibold, design: .rounded))
                 .kerning(-0.4)
         }
-        .foregroundStyle(.white)
+        .foregroundStyle(Color.white.opacity(0.65))
         .padding(.horizontal, 10)
         .padding(.vertical, 6)
         .background(Color.clear)
