532bb1dd81dec0f36bd03770b44706dd96428dc7
Hariharan Mudaliar <hrhn.mudaliar251@gmail.com>
2025-12-14 21:18:11 +0530

    Update App Name

--- DynamicIsland/models/Constants.swift
+++ DynamicIsland/models/Constants.swift
@@ -382,7 +382,7 @@ extension Defaults.Keys {
     static let menubarIcon = Key<Bool>("menubarIcon", default: true)
     static let showOnAllDisplays = Key<Bool>("showOnAllDisplays", default: false)
     static let automaticallySwitchDisplay = Key<Bool>("automaticallySwitchDisplay", default: true)
-    static let releaseName = Key<String>("releaseName", default: "alpha 0.0.1")
+    static let releaseName = Key<String>("releaseName", default: "Maldives")
     static let hideDynamicIslandFromScreenCapture = Key<Bool>("hideDynamicIslandFromScreenCapture", default: false)
     
         // MARK: Behavior
